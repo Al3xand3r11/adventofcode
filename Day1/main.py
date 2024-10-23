71aea5108194a67dc79c93f3b34479f15f417358
Al3xand3r11 <alexander.nance11@gmail.com>
2024-10-23 11:30:38 -0700

    day 1 fix

--- Day1/main.py
+++ Day1/main.py
@@ -6,6 +6,8 @@ with open('numbers.txt', 'r') as file:
         # Print each line
         if int(line) < 10:
             line += line
+        elif int(line) > 99:
+            line = line[0] + line[-1]
         num = float(line)
         total += num
     print(total)
