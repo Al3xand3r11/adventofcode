f63259091979fe86dbfd9ecec08b7327391db677
Al3xand3r11 <alexander.nance11@gmail.com>
2024-10-23 11:24:08 -0700

    first challenge

--- Day1/main.py
+++ Day1/main.py
@@ -1,4 +1,11 @@
-f = open("numbers.txt", "r")
-print(f.read())
-
-
+with open('numbers.txt', 'r') as file:
+    # Read each line in the file
+    total = 0
+    for line in file:
+        line="".join(c for c in line if  c.isdecimal())
+        # Print each line
+        if int(line) < 10:
+            line += line
+        num = float(line)
+        total += num
+    print(total)
